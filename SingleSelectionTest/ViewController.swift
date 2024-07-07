c737a6333d92842c7656665ccc12b040bfd5339c
hayate <hayate.k.0704@gmail.com>
2024-07-07 19:32:48 +0900

    add: initialize files

---
+++ SingleSelectionTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingleSelectionTest
+//
+//  Created by 柿崎 on 2024/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
